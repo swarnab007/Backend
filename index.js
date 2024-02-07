41e5be32a90c796f5b804c2ee292e13a5a479c2d
Swarnab Banerjee <116362013+swarnab007@users.noreply.github.com>
2024-02-07 17:41:17 +0530

    Backend commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// console.log("JavaScript Backend");
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = 7000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.get('/register', (req, res) => {
+    res.send("register page");
+})
+
+app.get('/login', (req, res) => {
+    res.send("<h3>login page</h3>");
+})
+
+app.get('/youtube', (req, res) => {
+    res.send("youtube page");
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+})
